6c30ba9558a8d2ab194b21d4ceade988d980358d
erick.merys <erick.soares>
2023-05-24 17:57:59 -0300

    API

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,9 +9,12 @@ import (
 )
 
 func main() {
+	config.Carregar()
+	fmt.Println(config.StringConexaoBanco)
+
 	fmt.Println("Rodando API!")
 	r:= router.Gerar()
 
 // Informar o porta e a rota
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d",config.Porta), r))
 }
